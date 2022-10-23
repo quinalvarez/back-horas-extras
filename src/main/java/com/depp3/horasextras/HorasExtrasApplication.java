f62b0c2217589f461020816f984a886a509a70a7
Joaquin Alvarez <joalvarez@infosis.com.ar>
2022-10-23 20:29:55 -0300

    chore: initial proyect

---
+++ src/main/java/com/depp3/horasextras/HorasExtrasApplication.java
@@ -0,0 +1,13 @@
+package com.depp3.horasextras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HorasExtrasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HorasExtrasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/depp3/horasextras/HorasExtrasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.depp3.horasextras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HorasExtrasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
